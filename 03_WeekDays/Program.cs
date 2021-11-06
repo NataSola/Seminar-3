c9035f2fd4c058049ac8be93f237b6416726bbaf
NataSola <nsol2009@yandex.ru>
2021-11-07 02:35:02 +0300

    Add week Days of the week

---
+++ 03_WeekDays/Program.cs
@@ -0,0 +1,25 @@
+﻿// 3. По введенному номеру дня недели вывести его название
+
+System.Console.WriteLine();
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+// int dayNumber = new Random().Next(1,8);
+
+System.Console.Write("Введите номер дня недели:   ");
+string writeDay = Console.ReadLine();
+
+int i = 0;
+bool result = int.TryParse(writeDay, out int dayNumber);
+// int dayNumber = Convert.ToInt32(writeDay);
+
+if (result == false) System.Console.WriteLine("Число введено неверно");
+else
+{
+    if (0 < dayNumber && dayNumber < 8) System.Console.WriteLine($"{dayNumber}-й день недели - {Days[dayNumber - 1]}");
+    else
+    {
+        System.Console.WriteLine("Дня недели с таким номером не существует");
+    }
+}
+
+System.Console.WriteLine();
