8851920bfc247835692cef0cde4fa3d785448bad
NataSola <nsol2009@yandex.ru>
2021-11-06 12:56:56 +0300

    Add comments

--- 01_NotSquare/Program.cs
+++ 01_NotSquare/Program.cs
@@ -1,4 +1,5 @@
 ﻿// 1. По двум введённым числам проверять является ли первое квадратом второго 
+
 System.Console.WriteLine();
 System.Console.WriteLine("Введите первое число: ");
 string writeNumber1 = Console.ReadLine();
@@ -8,11 +9,11 @@ System.Console.WriteLine("Введите второе число: ");
 string writeNumber2 = Console.ReadLine();
 int number2 = Convert.ToInt32(writeNumber2);
 
-string txtResult = $"Число {number1} не является квадратом числа {number2}";
+string txtResult = $"Число {number1} не является квадратом числа {number2}";     // вывод результат проверки на экран
 
-if (number1 == number2 * number2) 
+if (number1 == number2 * number2)               // можно записать (number1 / number2 == number2)
 {
-    txtResult = txtResult.Replace(" не", "");
+    txtResult = txtResult.Replace(" не", "");   // если условие выполняется, то частица "не" заменяется пустым значением
 }
 
 System.Console.WriteLine(txtResult);
