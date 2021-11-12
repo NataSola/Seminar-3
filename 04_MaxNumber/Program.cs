72304a56b8b9c6edf8f9e104a4cb8e9e3a193ce5
NataSola <nsol2009@yandex.ru>
2021-11-13 00:28:59 +0300

    Formating code

---
+++ 04_MaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// 4. Найти максимальное из трех чисел 
+
+int[] Numbers = { 54, 65, 3 };
+
+for (int i = 0; i < 3; i++)
+{
+    int max = Numbers[0];
+    
+    if (Numbers[i] > max)
+    {
+        max = Numbers[i];
+        System.Console.WriteLine();
+        System.Console.WriteLine($"Максимальное число {max}");
+    }
+}
+System.Console.WriteLine();
+
