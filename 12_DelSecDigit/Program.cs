08d1b1ae78ebd1d61be4bcece93609a0a07014d5
NataSola <nsol2009@yandex.ru>
2021-11-13 22:25:05 +0300

    Add deleting second digit of number

---
+++ 12_DelSecDigit/Program.cs
@@ -0,0 +1,19 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+System.Console.WriteLine();
+
+int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+
+int NumberWithoutSecDig()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+
+NumberWithoutSecDig();
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+System.Console.WriteLine();
