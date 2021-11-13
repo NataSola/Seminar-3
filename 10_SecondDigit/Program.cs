df55a6eba9d294131dc3166793da3a26c7e07d29
NataSola <nsol2009@yandex.ru>
2021-11-13 21:56:09 +0300

    Add the second digit of the number

---
+++ 10_SecondDigit/Program.cs
@@ -0,0 +1,9 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+Console.Clear();
+System.Console.WriteLine();
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+
+System.Console.WriteLine(); 
