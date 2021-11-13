5cc2719a1f0d989dbc8a764048e0cdbb6f09d077
NataSola <nsol2009@yandex.ru>
2021-11-13 22:28:50 +0300

    Add find third digit of number

---
+++ 14_Find3Number/Program.cs
@@ -0,0 +1,13 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+Console.Clear();
+System.Console.WriteLine();
+
+int Number = new Random().Next(1, 1500);
+System.Console.WriteLine();
+System.Console.WriteLine($"Дано число: {Number}");
+
+if (Number / 100 > 0) System.Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 100 % 10}");
+else System.Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+System.Console.WriteLine();
