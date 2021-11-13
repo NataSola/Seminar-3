9e6bf22c7447b9a787f032aa747f68b659a55165
NataSola <nsol2009@yandex.ru>
2021-11-13 20:43:10 +0300

    Add numbers from -N to N

---
+++ 07_NumersFromTo/Program.cs
@@ -0,0 +1,15 @@
+﻿// 7. Показать числа от -N до N
+
+Console.Clear();
+System.Console.Write("Введите число:   ");
+string writeNumber = Console.ReadLine();
+int endNumber = Convert.ToInt32(writeNumber);
+int Number = endNumber * (-1);
+System.Console.Write($"{Number} ");
+
+while (Number < endNumber)
+{
+    Number++;
+    System.Console.Write($"{Number} ");
+}
+System.Console.WriteLine(); System.Console.WriteLine();
