2215c850dab816f357ae8704e71c5baafd83fc91
NataSola <nsol2009@yandex.ru>
2021-11-14 02:41:29 +0300

    Add quarter number

---
+++ 19_QuarterNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+Console.Clear();
+System.Console.WriteLine();
+
+int QuarterNumber = 0;
+int x = new Random().Next(-100, 101);
+int y = new Random().Next(-100, 101);
+System.Console.WriteLine($"Координаты точки: ({x}, {y})");
+
+if (x > 0 && y > 0) QuarterNumber = 1;
+else if (x < 0 && y > 0) QuarterNumber = 2;
+else if (x < 0 && y < 0) QuarterNumber = 3;
+else QuarterNumber = 4;
+
+System.Console.WriteLine($"Точка с координатами ({x}, {y}) находится в {QuarterNumber}-й четверти");
+System.Console.WriteLine();
