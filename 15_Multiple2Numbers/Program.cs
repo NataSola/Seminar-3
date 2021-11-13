b191331dfd0540b208d7eb2aea2a354e79fad231
NataSola <nsol2009@yandex.ru>
2021-11-14 01:21:47 +0300

    Add square or

--- 15_Multiple2Numbers/Program.cs
+++ 15_Multiple2Numbers/Program.cs
@@ -2,15 +2,15 @@
 
 Console.Clear();
 int number = new Random().Next(1, 10000);
-// int number = 9;          для ручной проверки
+// int number = 9;                                  для ручной проверки
 int a = 7;
 int b = 23;
 string txtResult = " ";
 
-if (number % a == 0 && number % b == 0) txtResult = "кратно обоим числам";    
-else if (number % a != 0 && number % b != 0) txtResult = "не кратно ни одному из чисел"; 
-else if (number % a == 0) txtResult = $"кратно только числу {a}";  
-else txtResult = $"кратно только числу {b}"; 
+if (number % a == 0 && number % b == 0) txtResult = "кратно обоим числам";
+else if (number % a != 0 && number % b != 0) txtResult = "не кратно ни одному из чисел";
+else if (number % a == 0) txtResult = $"кратно только числу {a}";
+else txtResult = $"кратно только числу {b}";
 
 System.Console.WriteLine($"Проверить кратность числа {number} числам {a} и {b} ");
 System.Console.WriteLine();
---
+++ 17_SquareOrNot/Program.cs
@@ -0,0 +1,19 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+// !!! ПОВТОР ЗАДАЧИ № 2 !!!
+
+Console.Clear();
+System.Console.WriteLine();
+
+int[] num = new int[2];
+string Result = "не является";
+
+for (int i = 0; i < 2; i++)
+{
+    num[i] = new Random().Next(1, 1000);
+}
+
+if (num[0] / num[1] == num[1]) Result = Result.Replace("не ", "");
+
+System.Console.WriteLine($"Число {num[0]} {Result} квадратом числа {num[1]}");
+System.Console.WriteLine();
