4f2707c7175415face726de78e6b90c206fcd94c
NataSola <nsol2009@yandex.ru>
2021-11-14 00:41:31 +0300

    Add mulitple two numbers

---
+++ 15_Multiple2Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Clear();
+int number = new Random().Next(1, 10000);
+// int number = 9;          для ручной проверки
+int a = 7;
+int b = 23;
+string txtResult = " ";
+
+if (number % a == 0 && number % b == 0) txtResult = "кратно обоим числам";    
+else if (number % a != 0 && number % b != 0) txtResult = "не кратно ни одному из чисел"; 
+else if (number % a == 0) txtResult = $"кратно только числу {a}";  
+else txtResult = $"кратно только числу {b}"; 
+
+System.Console.WriteLine($"Проверить кратность числа {number} числам {a} и {b} ");
+System.Console.WriteLine();
+System.Console.WriteLine($"Число {number} {txtResult} ");
+System.Console.WriteLine();
+
+
