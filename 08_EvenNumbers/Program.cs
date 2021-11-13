121735d0f481a4d542955fe4434caa8fd75656da
NataSola <nsol2009@yandex.ru>
2021-11-13 20:59:27 +0300

    Add task 08 even numbers

---
+++ 08_EvenNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿// 8. Показать четные числа от 1 до N  
+
+Console.Clear();
+
+System.Console.Write("Введите число:   ");
+string txtN = Console.ReadLine();
+int endNum = Convert.ToInt32(txtN);
+int num = 0;
+System.Console.WriteLine($"Чётные числа из диапазона от 1 до {endNum}:");
+while (num < endNum)
+{
+    num++;
+    if (num % 2 == 0) System.Console.Write($"{num} ");
+}
+System.Console.WriteLine();System.Console.WriteLine();
