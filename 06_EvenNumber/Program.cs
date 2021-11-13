c0481a2d7a2f111f81a251e284e9325c90e8e406
NataSola <nsol2009@yandex.ru>
2021-11-13 20:28:49 +0300

    Add even number

---
+++ 06_EvenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// 6. Выяснить является ли число чётным  
+
+Console.Clear();
+System.Console.WriteLine();
+System.Console.Write("Введите число:    ");
+string writeNumber = Console.ReadLine();
+int number = Convert.ToInt32(writeNumber);
+string result = $"Число {number} является чётным";
+
+if (number % 2 != 0) result=result.Replace("является", "не является");
+
+System.Console.WriteLine(result);
+System.Console.WriteLine();
