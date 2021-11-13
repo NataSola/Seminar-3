686e61ab39ba5453348eb06045bf73c48edc40c5
NataSola <nsol2009@yandex.ru>
2021-11-13 21:15:27 +0300

    Add last digit of number

---
+++ 09_LastDigit/Program.cs
@@ -0,0 +1,9 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа 
+
+Console.Clear();
+System.Console.WriteLine();
+
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
+System.Console.WriteLine();
