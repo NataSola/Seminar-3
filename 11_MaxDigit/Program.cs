3604c860134b11fa1c3785dbfe38cda7e8683b4e
NataSola <nsol2009@yandex.ru>
2021-11-13 21:58:01 +0300

    Add max digit of number

---
+++ 11_MaxDigit/Program.cs
@@ -0,0 +1,21 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Clear();
+
+int Number = new Random().Next(10, 100);
+int Max = 0;
+
+int MaxDigit()
+{
+    int DigitOne = Number / 10;
+    int DigitTwo = Number % 10;
+    if (DigitOne > DigitTwo) Max = DigitOne;
+    else Max = DigitTwo;
+
+    return Max;
+}
+
+MaxDigit();
+System.Console.WriteLine();
+System.Console.WriteLine($"В числе {Number} наибольшая цифра {Max}");
+System.Console.WriteLine();
