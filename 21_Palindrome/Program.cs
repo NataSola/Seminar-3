e788fa86aee38a04492195ab3aeda6e960e07adc
NataSola <nsol2009@yandex.ru>
2021-11-14 15:38:23 +0300

    Add palindrome

--- 21_Palindrome/Program.cs
+++ 21_Palindrome/Program.cs
@@ -1,11 +1,16 @@
 ﻿// 21. Программа проверяет пятизначное число на палиндромом.
 
+// Задача решена двумя способами для любого целого числа
+
+// первый способ 
+
 Console.Clear();
 System.Console.WriteLine();
 
 System.Console.Write("Введите целое число:   ");
 string number = Console.ReadLine();
 int length = number.Length;
+int num = Convert.ToInt32(number);
 
 bool IsPalindrome()
 {
@@ -17,6 +22,35 @@ bool IsPalindrome()
     return false;
 }
 
-if (IsPalindrome()) System.Console.WriteLine($"Число {number} является палиндромом");
-else System.Console.WriteLine($"Число {number} не является палиндромом");
+System.Console.WriteLine("Ответ при решении первым способом:");
+if (IsPalindrome()) System.Console.WriteLine($"1. Число {number} является палиндромом");
+else System.Console.WriteLine($"1. Число {number} не является палиндромом");
+System.Console.WriteLine();
+
+
+// / второй способ    -- ограничено девятизначными числами
+
+int dev1 = 1;
+for (int i = 0; i < length - 1; i++)
+{
+    dev1 = dev1 * 10;
+}
+int dev2 = 10;
+
+bool IsPalindrome2()
+{
+    int numFirst = num / dev1;
+    int numSecond = num % dev2;
+    while (numFirst == numSecond)
+    {
+        return true;
+        dev1 = dev1 / 10;
+        dev2 = dev2 * 10;
+    }
+    return false;
+}
+
+System.Console.WriteLine("Ответ при решении вторым способом:");
+if (IsPalindrome2()) System.Console.WriteLine($"2. Число {number} является палиндромом");
+else System.Console.WriteLine($"2. Число {number} не является палиндромом");
 System.Console.WriteLine();
