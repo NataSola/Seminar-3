d19db759f8429e66832d92d0985ef940f3ff7f00
NataSola <nsol2009@yandex.ru>
2021-11-19 01:37:05 +0300

    Add distance between points

--- 22_PointsDistance/Program.cs
+++ 22_PointsDistance/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
+
+Console.Clear();
+System.Console.WriteLine();
+
+int numPlan = 3;
+int min = -5;
+int max = 5;
+
+int[] PointCoords(int numPlan)
+{
+    int[] coords = new int[numPlan];
+    for (int i = 0; i < numPlan; i++)
+    {
+        coords[i] = new Random().Next(min, max);
+    }
+    return coords;
+}
+
+void PrintCoords(int[] array)
+{
+    System.Console.Write($"({array[0]}, ");
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine($"{array[array.Length - 1]})");
+}
+
+double Distance(int[] arr1, int[] arr2)
+{
+    int sum = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        sum = sum + (arr1[i] - arr2[i]) * (arr1[i] - arr2[i]);
+    }
+    double result = Math.Sqrt(sum);
+    return result;
+}
+
+int[] PointA = PointCoords(numPlan);
+int[] PointB = PointCoords(numPlan);
+
+System.Console.Write($"Координаты точки А: ");
+PrintCoords(PointA);
+System.Console.Write($"Координаты точки B: ");
+PrintCoords(PointB);
+
+Distance(PointA, PointB);
+System.Console.WriteLine($"Раастояние между точками А и В: {Distance(PointA, PointB)}");
