67614d0c3c76f1169ee0dfc006c28ae22757daab
NataSola <nsol2009@yandex.ru>
2021-11-19 23:42:52 +0300

    Add rise to power

---
+++ 26_Exponentiation/Program.cs
@@ -0,0 +1,16 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+System.Console.WriteLine();
+
+int number = 2;
+int exponent = 10;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine(result);
