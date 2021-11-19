19bd95370fe4d84903ff7b2e63f7a0c54bc168ac
NataSola <nsol2009@yandex.ru>
2021-11-20 00:28:08 +0300

    Add sum of digits

---
+++ 27_CountNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите число:   ");
+string writeNum = Console.ReadLine();
+int number = Convert.ToInt32(writeNum);
+int result = number;
+
+// первый способ
+
+int count1 = 0;
+while (result > 0)
+{
+    result = result / 10;
+    count1++;
+}
+System.Console.WriteLine($"В числе {number} {count1} цифр");
+
+// второй способ
+
+int count2 = 0;
+for (result = number; result > 0; count2++) result = result /10;
+
+System.Console.WriteLine(count2);
---
+++ 28_DigitSum/Program.cs
@@ -0,0 +1,18 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+Console.Clear();
+System.Console.WriteLine();
+
+int number = new Random().Next(1, 100000);
+int sumDigit = 0;
+System.Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
+}
+
+System.Console.WriteLine(sumDigit);
+System.Console.WriteLine();
