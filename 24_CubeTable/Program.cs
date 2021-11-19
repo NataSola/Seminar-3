68e42296e08c288e8ee6d11a17ade34d436477aa
NataSola <nsol2009@yandex.ru>
2021-11-19 23:10:58 +0300

    Add numbers sum

---
+++ 24_CubeTable/Program.cs
@@ -0,0 +1,35 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите число:   ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+
+
+int[] GetCubeTable(int N)
+{
+    int[] cubs = new int[N];
+    int number = 1;
+    for (int i = 0; i < N; i++)
+    {
+        cubs[i] = number * number * number;
+        number++;
+    }
+    return cubs;
+}
+
+void PrintCubs(int[] array)
+{
+    int number = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{number} ^ 3 = ");
+        System.Console.WriteLine(array[i]);
+        number++;
+    }
+}
+
+int[] CubeTable = GetCubeTable(N);
+PrintCubs(CubeTable);
---
+++ 25_SumNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите число:   ");
+string writeA = Console.ReadLine();
+int A = Convert.ToInt32(writeA);
+
+int GetNumbersSum(int A)
+{
+    int number = 1;
+    int sum = 0;
+    for (int i = 0; i < A; i++)
+    {
+        sum = sum + number;
+        number++;
+    }
+    return sum;
+}
+
+int NumbersSum = GetNumbersSum(A);
+System.Console.WriteLine(NumbersSum);
