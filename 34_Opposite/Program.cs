133577a968e0c00a468f67041ff6457bb3c89464
NataSola <nsol2009@yandex.ru>
2021-11-20 21:38:56 +0300

    Add opposite array

---
+++ 34_Opposite/Program.cs
@@ -0,0 +1,36 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+
+// int n = new Random().Next(-100, 101);
+int n = 20;
+
+int[] CreateArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+    return array;
+}
+
+int[] GetOppositArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] array = CreateArray(n);
+PrintArray(array);
+PrintArray(GetOppositArray(array));
