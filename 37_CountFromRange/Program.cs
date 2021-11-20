3a83fba74a1b0f67b4edbbecda40fba86eb3dbf8
NataSola <nsol2009@yandex.ru>
2021-11-21 01:55:13 +0300

    Add count numbers from range

---
+++ 37_CountFromRange/Program.cs
@@ -0,0 +1,31 @@
+﻿// 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int size = 123;
+int min = -100;
+int max = 200;
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintCountArray(int[] arr)
+{
+    int count = 1;
+    System.Console.WriteLine();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+        if (arr[i] >= 10 && arr[i] <= 99) count++;
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Количество чисел из диапазона [10, 99] равно {count}");
+    System.Console.WriteLine();
+}
+
+PrintCountArray(FillArray(size, min,max));
