6a8fd772e655b5e7bafa1490d32dce2a0c807f98
NataSola <nsol2009@yandex.ru>
2021-11-20 16:56:30 +0300

    Add composition of numbers & rename Readme

---
+++ 29_CompositionNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите число:   ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+
+int GetNumbersComposition(int N)
+{
+    int comp = 1;
+    for (int num = 1; num <= N; num++)
+    {
+        comp = comp * num;
+    }
+    return comp;
+}
+
+System.Console.WriteLine($"Произведение чисел от 1 до {N} равно {GetNumbersComposition(N)}");
