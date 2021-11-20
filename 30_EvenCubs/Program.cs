a91639e3c946e5a86333b124d278bd93d8a6abfd
NataSola <nsol2009@yandex.ru>
2021-11-21 00:31:21 +0300

    Add method

--- 30_EvenCubs/Program.cs
+++ 30_EvenCubs/Program.cs
@@ -3,14 +3,47 @@
 Console.Clear();
 System.Console.WriteLine();
 
-int N = new Random().Next(1, 10);
+int N = new Random().Next(1, 20);
 int number = 1;
-
 System.Console.WriteLine($"N = {N}");
 
-while (number <=N)
+while (number <= N)
 {
-    int cube = number*number*number;
-    if (number%2 == 0) System.Console.WriteLine(cube);
+    int cube = number * number * number;
+    if (cube % 2 == 0) System.Console.Write(cube + " ");
     number++;
 }
+System.Console.WriteLine();
+
+// решение через метод
+
+int[] GetEvenCubeArray(int N)
+{
+    int num = 1;
+    int i = 0;
+    int[] EvenCubs = new int[N];
+    while (num <= N)
+    {
+        int cube = num * num * num;
+        if (cube % 2 == 0)
+        {
+            EvenCubs[i] = cube;
+            i++;
+        }
+        num++;
+    }
+    return EvenCubs;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine("By Method:");
+
+PrintArray(GetEvenCubeArray(N));
