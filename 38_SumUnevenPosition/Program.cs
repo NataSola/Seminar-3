8b199de77115d3c7c1315f621a592fd667007cbc
NataSola <nsol2009@yandex.ru>
2021-11-21 02:12:22 +0300

    Add count numders from uneven position

---
+++ 38_SumUnevenPosition/Program.cs
@@ -0,0 +1,31 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+Console.Clear();
+
+int size = 20;
+
+int[] FillArray(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    int sum = 0;
+    System.Console.WriteLine();
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write(arr[i]+" ");
+        if (i % 2 > 0) sum += arr[i];
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Сумма чисел, стоящих на нечетной позиции, соcтаваляет {sum} ");
+    System.Console.WriteLine();
+}
+
+PrintArray(FillArray(size));
