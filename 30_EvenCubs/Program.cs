303232c849dd1ba9a09a9ea405667b88d1e51af1
NataSola <nsol2009@yandex.ru>
2021-11-21 01:27:11 +0300

    Add Method 2

--- 30_EvenCubs/Program.cs
+++ 30_EvenCubs/Program.cs
@@ -3,10 +3,13 @@
 Console.Clear();
 System.Console.WriteLine();
 
-int N = new Random().Next(1, 20);
-int number = 1;
+int N = new Random().Next(1, 30);
 System.Console.WriteLine($"N = {N}");
 
+// решение через цикл 
+
+int number = 1;
+System.Console.WriteLine("By While");
 while (number <= N)
 {
     int cube = number * number * number;
@@ -16,6 +19,8 @@ while (number <= N)
 System.Console.WriteLine();
 
 // решение через метод
+
+System.Console.WriteLine("By Method 1:");
 
 int[] GetEvenCubeArray(int N)
 {
@@ -44,6 +49,31 @@ void PrintArray(int[] arr)
     System.Console.WriteLine();
 }
 
-System.Console.WriteLine("By Method:");
-
 PrintArray(GetEvenCubeArray(N));
+
+// еще один способ: собрать массив из кубов, а на печать вывести только четные
+
+System.Console.WriteLine("By Method 2");
+
+int[] GetCubeArray(int N)
+{
+    int i = 0;
+    int[] EvenCubs = new int[N];
+    for (int num = 1; num <= N; num++)
+    {
+        EvenCubs[i] = num * num * num;
+        i ++;
+    }
+    return EvenCubs;
+}
+
+void PrinEvenCubes(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+PrinEvenCubes(GetCubeArray(N));
+System.Console.WriteLine();
