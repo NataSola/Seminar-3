1f69f1724bc38db7fb28365467055427f5905d72
NataSola <nsol2009@yandex.ru>
2021-11-20 20:07:40 +0300

    Add aarays of 8 elements

---
+++ 31_Array8Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран
+
+int N = 8;
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1,100);
+    }
+    return arr;
+}
+
+void PrintArray(int[]array)
+{
+    for(int i=0; i<array.Length; i++)
+    System.Console.Write($"{array[i]} ");
+}
+
+PrintArray(SetArray(N));
---
+++ 32_Array8_0_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int N = 8;
+int min = 0;
+int max = 1;
+
+int[] CreateArray(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(CreateArray(N, min, max));
